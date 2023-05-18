0f967fddf44529623423f14c7a86d2cc3477d6fa
MichaelMauri03 <michael.mauri@ptpvenezia.edu.it>
2023-05-18 11:14:11 +0200

    #

--- back end/dashboard società.php
+++ back end/dashboard società.php
@@ -1,12 +1,9 @@
-<!-- Dashboard Società -->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard Società</title>
-  <!-- Collegamento a Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
@@ -96,7 +93,6 @@
       </div>
     </div>
 
-    <!-- Script per popolare il modulo con le statistiche del calciatore selezionato -->
     <script>
     // Funzione per aprire il modulo di modifica statistiche
     function apriModificaStatisticheModal(id_giocatore) {
@@ -124,7 +120,6 @@
     }
     </script>
 
-    <!-- Collegamento a jQuery e Bootstrap JavaScript -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   </div>
